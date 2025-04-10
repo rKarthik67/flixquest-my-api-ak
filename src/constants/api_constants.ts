e7845cf53cb7f9148416e43dc7ee540b42fdf874
KARTHIKEYAN R <90916934+rKarthik67@users.noreply.github.com>
2025-04-10 14:52:29 +0530

    Update api_constants.ts

--- src/constants/api_constants.ts
+++ src/constants/api_constants.ts
@@ -2,7 +2,7 @@ import dotenv from "dotenv";
 dotenv.config();
 
 export const tmdbBaseUrl = "https://api.themoviedb.org";
-export const tmdbKey = '5a917d25f7c40ac92b4317c99a46600d';
+export const tmdbKey = "5a917d25f7c40ac92b4317c99a46600d";
 export const daddyliveReferrer = "https://lewblivehdplay.ru/";
 export const daddyliveUserAgent =
     "Mozilla/5.0 (iPhone; CPU iPhone OS 17_4_1 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/17.4.1 Mobile/15E148 Safari/604.1";
